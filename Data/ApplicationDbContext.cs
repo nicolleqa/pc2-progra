8769c7da8f0e2568d46f12e23b32c2d1f64c2a97
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2025-05-02 10:44:55 -0500

    add modelos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using pc2_progra.Models;
+using System.Collections.Generic;
 
 namespace pc2_progra.Data;
 
@@ -8,5 +10,19 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<Pets> Pets { get; set; }
+    public DbSet<Adopters> Adopters { get; set; }
+    public DbSet<Adoptions> Adoptions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Pets>()
+            .HasOne(p => p.Adoption)
+            .WithOne(a => a.Pet)
+            .HasForeignKey<Adoptions>(a => a.PetId);
     }
 }
